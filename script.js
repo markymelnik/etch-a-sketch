dfb6264291a55da3704328fe32068f02a47084a7
markymelnik <markemelnik@gmail.com>
2022-11-07 13:11:15 -0500

    Created static, 16x16 square grid

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let container = document.getElementById('container');
-container.innerHTML = "container";
 
-let smallBox = document.createElement('div');
-smallBox.id = 'smallBox';
-smallBox.innerHTML = "box";
+window.onload = function() {
+    let count = 16,
+    etchGrid = document.getElementById('container'),
+    etchBox,
+    etchFragment = document.createDocumentFragment();
 
-container.appendChild(smallBox);
+    for (let i = 1; i <= count; i++) {
+        for (let j = 1; j <= count; j++) {
+            etchBox = document.createElement('div');
+            etchBox.id = 'etchBox';
+            etchFragment.appendChild(etchBox);
+        }
+    }
+    etchGrid.appendChild(etchFragment);
+}
 
