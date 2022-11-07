f11e8d6ec24916ceffdb18a41c4c87132eddbb04
markymelnik <markemelnik@gmail.com>
2022-11-07 17:03:49 -0500

    Created mousedown, mouseup, and mouseover events which allow sketching on the grid

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 
-const container = document.querySelector('.container');
+const etchContainer = document.querySelector('.etchContainer');
 
+getGrid(16);
+
+const etchBox = document.querySelectorAll('.etchBox');
+
+let isMouseClicked = false;
+
+etchBox.forEach(box => {
+    box.addEventListener('mousedown', () => {
+        isMouseClicked = true;
+    })
+});
+
+etchBox.forEach(box => {
+    box.addEventListener('mouseup', () => {
+        isMouseClicked = false;
+    })
+});
+
+etchBox.forEach(box => {
+    box.addEventListener('mouseover', () => {
+        if (isMouseClicked == true) {
+            box.style.background = 'black';
+        }
+    })
+});
 
 function getGrid(dimension) {
-    container.style.gridTemplateColumns = `repeat(${dimension}, auto)`;
+    etchContainer.style.gridTemplateColumns = `repeat(${dimension}, auto)`;
     for (let i = 1; i <= (dimension * dimension); i++) {
         const etchBox = document.createElement('div');
         etchBox.classList.add('etchBox');
-        container.appendChild(etchBox);
+        etchContainer.appendChild(etchBox);
     }
 };
-
-getGrid(16);
